2ec1d7b9d6d97a6dd3f381b67706e329f84b2229
miguel <usmo300@hotmail.com>
2021-02-24 15:16:17 +0100

    prueba de routes

---
+++ src/router/index.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Home.vue')
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/componente',
+    name: 'componente',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Componente.vue')
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
---
+++ tests/unit/test.spec.js
@@ -0,0 +1,7 @@
+//import {createLocalVue, mount} from '@vue/test-utils'
+//import {plugin} from '@/components/plugin.vue'
+
+//const localVue= createLocalVue()
+//localVue.use(plugin)
+
+//mount(component, {localVue})
